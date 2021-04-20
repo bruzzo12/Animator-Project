543a1790262d3c8ef0275eb9d0ce497760592d08
Rudy <46633698+rmesa19@users.noreply.github.com>
2021-04-20 18:34:45 -0400

    Updated Visual View fields updating AnimatorPanel for visual view Implemented Tween

--- src/cs5004/animator/view/AnimationPanel.java
+++ src/cs5004/animator/view/AnimationPanel.java
@@ -18,7 +18,7 @@ class AnimationPanel extends JPanel {
   public int endValue;
   public int startTime;
   public int endTime ;
-  private int currentTime;
+  public int currentTime;
   /**
    * Constructs an Animation Panel object.
    */
@@ -32,30 +32,35 @@ class AnimationPanel extends JPanel {
     this.startTime = startTime;
     this.endTime = endTime;
     currentTime = 0;
-
+System.out.println(currentTime);
 
     new Timer(this.speedValue, new ActionListener() {
-
+      //System.out.println(String.format("Current time is: %d\n", currentTime));
       @Override
-      public void actionPerformed(ActionEvent e) {
+      public void actionPerformed(ActionEvent actEvt) {
+        System.out.println(String.format("Current time is: %d\n", currentTime));
         if(currentTime >= startTime){
           System.out.println(String.format("Current time is: %d\n", currentTime));
           currentValue = tween(startValue, endValue, startTime, endTime, currentTime);
+
           repaint();
 
         }
         if(currentTime == endTime){
-          ((Timer)e.getSource()).stop();
+          ((Timer)actEvt.getSource()).stop();
         }
         currentTime++;
       }
-    });
+    }).start();
   }
 
-  private int tween(int startValue, int endValue, int startTime, int endTime, int currentTime){
-    return startValue*((endTime - currentTime)/(endTime-startTime)) +
-            endValue*((currentTime - startTime)/(endTime-startTime));
-  };
+  public static int tween(int startValue, int endValue, int startTime, int endTime, int currentTime) {
+    return (int)(startValue * (((double)endTime - (double)currentTime) /
+            ((double)endTime - (double)startTime))) +
+            (int)(endValue * (((double)currentTime - (double)startTime) /
+                    ((double)endTime - (double)startTime)));
+  }
+
 
   public Dimension getPreferredSize() {
     return new Dimension(500, 500);
@@ -68,16 +73,18 @@ class AnimationPanel extends JPanel {
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
+    Graphics2D g2d = (Graphics2D) g.create();
+    System.out.println(String.format("CurrentValue is :%d Current time is:%d",currentValue, currentTime));
       Color objectColor = new Color(currentValue, 12, 45);
-      g.setColor(objectColor);
-      g.drawRect(300, 300, currentValue, 12);
-      g.fillRect(300, 300, currentValue, 12);
+      g2d.setColor(objectColor);
+      g2d.drawRect(currentTime, 300, currentValue, 12);
+      g2d.fillRect(currentTime, 300, currentValue, 12);
 
 
 
   }
   private static void setupGUI(){
-    AnimationPanel mainPanel = new AnimationPanel(1,"Rectangle",
+    AnimationPanel mainPanel = new AnimationPanel(30,"Rectangle",
             11,30,0, 50);
     JFrame frame = new JFrame("Shapes");
     frame.setPreferredSize(new Dimension(700,700));
